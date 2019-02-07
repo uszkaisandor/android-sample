5c9d8dd99e13c5b40415a39ae9e8bbf9f5fcd95f
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-07 12:20:22 +0100

    Create UI layouts

---
+++ app/src/main/java/com/example/trainingproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trainingproject
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
