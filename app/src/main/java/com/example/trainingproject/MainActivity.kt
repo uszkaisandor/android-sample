a61878e665f7f2b30d9a94232ddedb6e4971f389
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-07 13:20:31 +0100

    Change layouts to AndroidX version

--- app/src/main/java/com/example/trainingproject/MainActivity.kt
+++ app/src/main/java/com/example/trainingproject/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.trainingproject
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
