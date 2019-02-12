095a98ebf4a19cc63fd9a43ed9fd06085b10f81c
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-12 10:41:06 +0100

    Add empty listeners to buttons

--- app/src/main/java/com/example/trainingproject/MainActivity.kt
+++ app/src/main/java/com/example/trainingproject/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.trainingproject
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.trainingproject.service.RandomColor
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,19 +22,26 @@ class MainActivity : AppCompatActivity() {
         RandomColor.setButtonBackground(btnTabSecond)
         RandomColor.setButtonBackground(btnTabThird)
 
-        /*
+        btnTabFirst.setOnClickListener{
+        }
 
-        var randomValues = List(9) { Random.nextInt(0, 256) }
+        btnTabSecond.setOnClickListener{
+        }
 
-        val buttonTabFirstColor: Int =  Color.argb(255, randomValues[0], randomValues[1], randomValues[2])
-        btnTabFirst.setBackgroundColor(buttonTabFirstColor)
+        btnTabThird.setOnClickListener{
+        }
 
-        val buttonTabSecondColor: Int =  Color.argb(255, randomValues[3], randomValues[4], randomValues[5])
-        btnTabSecond.setBackgroundColor(buttonTabSecondColor)
+        iconButtonLeft.setOnClickListener{
 
-        val buttonTabThirdColor: Int =  Color.argb(255, randomValues[6], randomValues[7], randomValues[8])
-        btnTabThird.setBackgroundColor(buttonTabThirdColor)*/
+        }
 
+        iconButtonCenter.setOnClickListener{
+
+        }
+
+        iconButtonRight.setOnClickListener{
+
+        }
 
     }
 }
